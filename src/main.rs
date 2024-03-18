9f43cef11af061b9019fd57c7f02ad9673149a1b
Envixty <114357508+redkittty@users.noreply.github.com>
2024-03-18 16:40:28 -0400

    v2.0 Source Code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::io;
 fn main() {
-    println!("SimpleRustCalculator");
-    println!("Modes = (1) = Addition, (2) = Subtraction, (3) = Multiplication, (4) = Division");
+    println!("SimpleRustCalculator v2.0");
+    println!("Licensed under GNU General Public License v3.0 (GPLv3)");
+    println!("Modes = (1) = Addition, (2) = Subtraction, (3) = Multiplication, (4) = Division, (5) = Squared");
     let mut operator = String::new();
     // Allows user to input stuff
     io::stdin()
         .read_line(&mut operator)
         .expect("Failed to read line");
     let operator: i8 = operator.trim().parse().expect("Please insert a number");
-    if operator == 1 || operator == 2 || operator == 3 || operator == 4 {
+    if operator == 1 || operator == 2 || operator == 3 || operator == 4 || operator == 5 {
         // Valid operator
         println!("-----");
         // First Number
@@ -17,32 +18,37 @@ fn main() {
             .read_line(&mut num1)
             .expect("Failed to read line");
         let num1: f64 = num1.trim().parse().expect("Please insert a number");
-        // Secpmd Number
-        let mut num2 = String::new();
-        io::stdin()
-            .read_line(&mut num2)
-            .expect("Failed to read line");
-        let num2: f64 = num2.trim().parse().expect("Please insert a number");
-        // Operators
-        // Addition
-        if operator == 1 {
-            let sum = num1 + num2;
-            println!("= {sum}");
+        // Squared
+        if operator == 5 {
+            let sqred = num1 * num1;
+            println!("= {sqred}");
         }
-        // Subtraction
-        if operator == 2 {
-            let dif = num1 - num2;
-            println!("= {dif}");
-        }
-        // Multiplication
-        if operator == 3 {
-            let mul = num1 * num2;
-            println!("= {mul}");
-        }
-        // Division
-        if operator == 4 {
-            let div = num1 / num2;
-            println!("= {div}");
+        else {
+            // Second Number
+            let mut num2 = String::new();
+            io::stdin()
+                .read_line(&mut num2)
+                .expect("Failed to read line");
+            let num2: f64 = num2.trim().parse().expect("Please insert a number");
+            // Operations
+            // Addition
+            if operator == 1 {
+                let sum = num1 + num2;
+                println!("= {sum}");
+            }
+            // Subtraction
+            if operator == 2 {
+                let dif = num1 - num2;
+                println!("= {dif}");
+            }
+            if operator == 3 {
+                let mul = num1 * num2;
+                println!("= {mul}");
+            }
+            if operator == 4 {
+                let div = num1 / num2;
+                println!("= {div}");
+            }
         }
     }
     else {
