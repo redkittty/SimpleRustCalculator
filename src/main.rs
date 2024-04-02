f3223bf99976e6c63dd3f539678284b174df0fc4
redkittty <114357508+redkittty@users.noreply.github.com>
2024-04-02 19:02:25 -0400

    Added Pythagrean Theorum

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use std::io;
 fn main() {
     println!("SimpleRustCalculator v3.0");
     println!("Licensed under GNU General Public License v3.0 (GPLv3)");
-    println!("Modes = (1) = Addition, (2) = Subtraction, (3) = Multiplication, (4) = Division, (5) = Squared, (6) = Sq Root");
+    println!("Modes = (1) = Addition, (2) = Subtraction, (3) = Multiplication, (4) = Division, (5) = Squared, (6) = Sq Root, (7) = Pythagrean Theorum");
     let mut operator = String::new();
     // Allows user to input stuff
     io::stdin()
         .read_line(&mut operator)
         .expect("Failed to read line");
     let operator: i8 = operator.trim().parse().expect("Please insert a number");
-    if operator == 1 || operator == 2 || operator == 3 || operator == 4 || operator == 5 || operator == 6 {
+    if operator == 1 || operator == 2 || operator == 3 || operator == 4 || operator == 5 || operator == 6 || operator == 7 {
         // Valid operator
         println!("-----");
         // First Number
@@ -54,6 +54,34 @@ fn main() {
             if operator == 4 {
                 let div = num1 / num2;
                 println!("= {div}");
+            }
+            // Pythagrean Theorum
+            if operator == 7 {
+                let sq1 = num1 * num1;
+                let sq2 = num2 * num2;
+                println!("Is there a missing leg?");
+                println!("(1) = yes, (2) = no,");
+                let mut a = String::new();
+                io::stdin()
+                    .read_line(&mut a)
+                    .expect("Failed to Read Line");
+                let a: i8 = a.trim().parse().expect("Please insert a number");
+                if a == 1 || a == 2 {
+                    if a == 1 {
+                        let pyt1 = sq1 - sq2;
+                        let pyt2 = f64::sqrt(pyt1);
+                        println!("= {pyt2}");
+                    }
+                    if a == 2 {
+                        let pyt1 = sq1 + sq2;
+                        let pyt2 = f64::sqrt(pyt1);
+                        println!("= {pyt2}");
+                    }
+                }
+                // Invalid Option
+                else {
+                    println!("Invalid Option, Please try again");
+                }
             }
         }
     }
